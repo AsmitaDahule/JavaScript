c134327b1eac3d5898afe0f4235fb0892afb8551
Asmita Dahule <dahuleasmita@gmail.com>
2024-12-19 21:23:38 +0530

    functions

---
+++ 13_functions.js
@@ -0,0 +1,39 @@
+function greeting(){
+    console.log("Hare Krishna Dostooo")
+} 
+
+greeting()
+
+
+
+function sum(num1,num2){
+    result = num1+num2;
+    return result;
+}
+
+sum(2,5);
+console.log(sum(2,5))
+
+
+
+function userLogin(name="kittu"){
+    return `${name} just logged in`
+}
+ console.log(userLogin("anupama"))
+
+
+
+function mul(m1,m2){
+    result = m1 * m2
+    console.log(result)
+} 
+
+mul(5,8);
+
+
+
+function sub(n1,n2){
+    console.log(n1 - n2)
+}
+
+sub(5,6);
