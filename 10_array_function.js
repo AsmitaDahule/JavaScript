fd1f74991e440e52e0dae8f04c3918461499db5e
Asmita Dahule <dahuleasmita@gmail.com>
2024-12-19 17:33:09 +0530

    arrays

--- 10_array_function.js
+++ 10_array_function.js
@@ -1,21 +1,21 @@
 const marvel_heros = ["thor", "Ironman", "spiderman"];
 const dc_heros = ["superman", "flash","batman"];
 
-// marvel_heros.push(dc_heros);
-// console.log(marvel_heros);  // array inside a array
-// console.log(marvel_heros[3][0]) // superman
+marvel_heros.push(dc_heros);
+console.log(marvel_heros);  // array inside a array
+console.log(marvel_heros[3][0]) // superman
 
-// const all_hero = marvel_heros.concat(dc_heros)
-// console.log(all_hero)  // merge in a single array
-
-
-// const all_new_heros = [...marvel_heros, ...dc_heros]  // spread operator (no limitation)
-// console.log(all_new_heros)  // same as concat
+const all_hero = marvel_heros.concat(dc_heros)
+console.log(all_hero)  // merge in a single array
 
 
-// const another_array = [1,2,3,[4,5,6,[5,8,[9,8]]]]
-// const real_another_array = another_array.flat(Infinity)
-// console.log(real_another_array)
+const all_new_heros = [...marvel_heros, ...dc_heros]  // spread operator (no limitation)
+console.log(all_new_heros)  // same as concat
+
+
+const another_array = [1,2,3,[4,5,6,[5,8,[9,8]]]]
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array)
 
 
 console.log(Array.isArray("mekochan"))
